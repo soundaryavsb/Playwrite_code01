b01743333263726edae7fbe022e1c999494f7a54
soundaryavsb <dsoundarya1998@gmail.com>
2026-08-10 15:31:43 +0530

    async,await and fixture

--- tests/First_script.spec.js
+++ tests/First_script.spec.js
@@ -1,5 +1,15 @@
+// import {test} from "@playwright/test"
+// import { log } from "node:console"
+// test("first code",()=>{
+//     console.log("Hello");
+// })
+
 import {test} from "@playwright/test"
-import { log } from "node:console"
-test("first code",()=>{
-    console.log("Hello");
+import { log } from "node:console";
+test("first code",async()=>{
+    await console.log("Hello");
+})
+
+test("browser name",async({browserName})=>{
+    console.log(browserName);  
 })
