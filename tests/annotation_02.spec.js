19898d963c59a3e7bfb2494af2dcd7bb4d2ec41e
soundaryavsb <dsoundarya1998@gmail.com>
2026-08-11 08:03:39 +0530

    added annotations only

---
+++ tests/annotation_02.spec.js
@@ -0,0 +1,17 @@
+import {test} from "@playwright/test"
+
+test("anno1",()=>{
+    console.log("annotation 1");
+})
+test.only("anno2",()=>{
+    console.log("annotation 2");
+})
+test("anno3",()=>{
+    console.log("annotation 3");
+})
+test("anno4",()=>{
+    console.log("annotation 4");
+})
+test("anno5",()=>{
+    console.log("annotation 5");
+})
