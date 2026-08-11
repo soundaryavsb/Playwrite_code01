c9b8d8c40947e8b3af593044ccbf71bce52ca15e
soundaryavsb <dsoundarya1998@gmail.com>
2026-08-11 07:41:47 +0530

    annotation fail is executed without fail

---
+++ tests/annotation.spec.js
@@ -0,0 +1,25 @@
+import {test} from "@playwright/test"
+
+//browser fixture
+test.fail("fixtures",async({browser})=>{ //tells the browser
+    let context=await browser.newContext(); //browser session(isolated session(profile), incognito mode)
+    let page=await context.newPage(); //tab within the browser session
+    await page.goto("https://www.amazon.in/");
+    await page.waitForTimeout(3000);
+}) //control to multiple context(browser session) multiple windows
+
+//context
+test("context fixture",async({context})=>{ //default it will create browser session in the all the 3 browser
+    let page=await context.newPage();
+    await page.goto("https://www.amazon.in/");
+}) //control over multiple tabs within one session
+
+// page
+test("page fixture",async({page})=>{ //tab inside the default 3 browsers
+    await page.goto("https://www.amazon.in/");
+})//control over a single tab
+
+//browsername
+test.skip("browsername fixture",async({browserName})=>{
+    console.log(browserName);
+}) //return the name of the browser which is in used
