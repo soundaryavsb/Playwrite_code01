70e1f939fafc35622c9dd27ba85de397cec1a7af
soundaryavsb <dsoundarya1998@gmail.com>
2026-08-13 12:40:27 +0530

    create account and register

--- tests/shoppersstackLogin.spec.js
+++ tests/shoppersstackLogin.spec.js
@@ -4,5 +4,14 @@ test("shoppersstack Login",async ({page}) => {
     await page.goto("https://shoppersstack.com/");
     await page.locator('#loginBtn').click();
     await page.locator('button[id="Create Account"]').click();
-    await page.waitForTimeout(2000);
+    await page.locator('input[id="First Name"]').fill("Arya");
+    await page.locator('input[id="Last Name"]').fill("Raya");
+    await page.locator('#Female').check();
+    await page.locator('input[id="Phone Number"]').fill("9922334455");
+    await page.locator('input[id="Email Address"]').fill("S9922334455@gmail.com");
+    await page.locator('input[id="Password"]').fill("S99999999%f");
+    await page.locator('input[id="Confirm Password"]').fill("S99999999%f");
+    await page.locator('input[id="Terms and Conditions"]').check();
+    await page.locator('button[id="btnDisabled"]').click();
+    await page.waitForTimeout(3000);
 })
