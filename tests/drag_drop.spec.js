90bf3ebb3d5e506626f9f7c7a2c82f139fa5c9c3
soundaryavsb <dsoundarya1998@gmail.com>
2026-08-24 08:10:37 +0530

    drag and drop to target

--- tests/drag_drop.spec.js
+++ tests/drag_drop.spec.js
@@ -12,3 +12,32 @@ test("drag and drop",async ({page}) => {
     await page.mouse.up();
     await page.waitForTimeout(3000);
 })
+
+//* drag and drop to target using mouse action:
+test.only("drag and drop to target",async ({page}) => {
+    await page.goto("https://demoapps.qspiders.com/ui/dragDrop/dragToCorrect?sublist=2");
+    //! Mobile Charger to Mobile Accessories
+    await page.locator("//div[text()='Mobile Charger']").hover();
+    await page.mouse.down();
+    await page.locator("//div[text()='Mobile Accessories']").hover();
+    await page.mouse.up();
+    await page.waitForTimeout(3000);
+    //! Mobile Cover to Mobile Accessories
+    await page.locator("//div[text()='Mobile Cover']").hover();
+    await page.mouse.down();
+    await page.locator("//div[text()='Mobile Accessories']").hover();
+    await page.mouse.up();
+    await page.waitForTimeout(3000);
+    //! Laptop Charger to Laptop Accessories
+    await page.locator("//div[text()='Laptop Charger']").hover();
+    await page.mouse.down();
+    await page.locator("//div[text()='Laptop Accessories']").hover();
+    await page.mouse.up();
+    await page.waitForTimeout(3000);
+    //! Laptop Cover to Laptop Accessories
+    await page.locator("//div[text()='Laptop Cover']").hover();
+    await page.mouse.down();
+    await page.locator("//div[text()='Laptop Accessories']").hover();
+    await page.mouse.up();
+    await page.waitForTimeout(3000);
+})
