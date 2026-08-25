e9f66a79c775af6fba1bc845eab3de8e1666c11e
soundaryavsb <dsoundarya1998@gmail.com>
2026-08-25 07:32:03 +0530

    dragTo method

--- tests/drag_drop.spec.js
+++ tests/drag_drop.spec.js
@@ -79,14 +79,15 @@ test("drag and drop to target using bounding box",async ({page}) => {
 //* drag and drop to target using dragTo():
 test.only("drag and drop using method",async ({page}) => {
     await page.goto("https://demoapps.qspiders.com/ui/dragDrop/dragToCorrect?sublist=2");
-    //! To Mobile Accessories
+    //! To Mobile Accessories:
     let Mobile_Charger=await page.locator("//div[text()='Mobile Charger']");
     let Mobile_Cover=await page.locator("//div[text()='Mobile Cover']");
     let Mobile_Accessories=await page.locator("//div[text()='Mobile Accessories']");
-    //! To Laptop Accessories
+    //! To Laptop Accessories:
     let Laptop_Charger=await page.locator("//div[text()='Laptop Charger']");
     let Laptop_Cover=await page.locator("//div[text()='Laptop Cover']");
     let Laptop_Accessories=await page.locator("//div[text()='Laptop Accessories']");
+    //! dragTo():
     await Mobile_Charger.dragTo(Mobile_Accessories);
     await Mobile_Cover.dragTo(Mobile_Accessories);
     await Laptop_Charger.dragTo(Laptop_Accessories);
