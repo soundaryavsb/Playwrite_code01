48a666b8ece66504db2896aadd494b9d873d6953
soundaryavsb <dsoundarya1998@gmail.com>
2026-08-26 08:50:04 +0530

    dropdown - multiple select

--- tests/dropdowns.spec.js
+++ tests/dropdowns.spec.js
@@ -1,7 +1,7 @@
 import {test} from "@playwright/test"
 
 //* standard drop down
-test("select",async ({page}) => {
+test("Standard select",async ({page}) => {
     await page.goto("https://demoapps.qspiders.com/ui/dropdown?sublist=0");
     //* selecting through value
     //country
@@ -18,4 +18,18 @@ test("select",async ({page}) => {
     await page.locator("//label[text()='City']/following-sibling::select").selectOption({index:1}); //!using Xpath
     // await page.locator("select[class='border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150']").nth(2).selectOption({index:1}); //!using CSS selector
     await page.waitForTimeout(3000);
+})
+
+//* Multiple drop down
+test.only("Multiple select",async ({page}) => {
+    await page.goto("https://demoapps.qspiders.com/ui/dropdown/multiSelect?sublist=1");
+    //select multiple option //!Note: store it in array
+    await page.locator("select[id='select-multiple-native']")
+            .selectOption([
+                   {value:"Mens Casual Premium Slim Fit T-Shirts "},
+                   {value:"Mens Cotton Jacket"}])
+    //click add button
+    await page.locator("button[class='bg-orange-500 p-2 text-white rounded w-[150px]']").click();
+    await page.waitForTimeout(3000);
+
 })
