114f2bf3f735e001ea6b1fb29883b4341257f11a
soundaryavsb <dsoundarya1998@gmail.com>
2026-08-30 21:02:26 +0530

    window_handles

--- tests/window_handles.spec.js
+++ tests/window_handles.spec.js
@@ -11,3 +11,38 @@ test("New Tab",async ({browser}) => {
     await page2.waitForTimeout(3000);
     
 })
+test("window handle",async ({browser}) => {
+    let context=await browser.newContext();
+    let page=await context.newPage();
+    await page.goto("https://www.amazon.in/s?k=shirt&crid=3AXQOTXVR318R&sprefix=shirt%2Caps%2C286&ref=nb_sb_noss_2");
+    await page.waitForTimeout(3000);
+    page.getByText("Men's Checkered Casual Shirt - Regular Fit | Full Sleeve | Pure Cotton (Available in Plus Sizes)']").click();
+    await page.waitForTimeout(3000);
+})
+test("window handle 2",async ({browser}) => {
+    let context=await browser.newContext();
+    let page=await context.newPage();
+    await page.goto("https://www.amazon.in/s?k=shirt&crid=3AXQOTXVR318R&sprefix=shirt%2Caps%2C286&ref=nb_sb_noss_2");
+    await page.waitForTimeout(3000);
+    page.getByText("Men's Checkered Casual Shirt - Regular Fit | Full Sleeve | Pure Cotton (Available in Plus Sizes)']").click();
+    await page.waitForTimeout(3000);
+})
+
+
+//Assignment 1: //!done
+test("Qspider Multiple window",async ({browser}) => {
+    let context=await browser.newContext();
+    let page=await context.newPage();
+    await page.goto("https://demoapps.qspiders.com/ui/browser/multipleWindow?sublist=2");
+    // await page.waitForTimeout(3000);
+    let [newpage]=await Promise.all([
+        page.waitForEvent("popup"),
+        page.locator("//button[text()='Shop Now']").click()
+    ])
+    // await newpage.waitForTimeout(3000);
+    await newpage.locator("//button[text()='Add to Cart']").click();
+    await newpage.locator("//button[text()='Add to Cart']").click();
+    let nOfProducts=await newpage.locator("//section[@class='relative']/article").textContent();
+    console.log(nOfProducts);
+    // await newpage.waitForTimeout(3000);
+})
