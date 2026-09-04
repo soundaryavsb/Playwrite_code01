136bdf86a1c1667d627bbea7601a9dc3f8d0fb77
soundaryavsb <dsoundarya1998@gmail.com>
2026-09-04 08:33:18 +0530

    Data Driven - Multiple data - Column constant

--- tests/multipleddt.spec.js
+++ tests/multipleddt.spec.js
@@ -2,7 +2,7 @@ import {test} from "@playwright/test"
 import excel from "exceljs"
 import path from "node:path"
 
-//*fetching multiple data
+//*fetching multiple data - keeping Row constant
 test("fetching multiple data",async ({page}) => {
     let book=await new excel.Workbook();
     await book.xlsx.readFile(path.join(__dirname,"../Data Driven Testing/multipledata.xlsx"))
@@ -12,5 +12,16 @@ test("fetching multiple data",async ({page}) => {
         let data=await sheet.getRow(1).getCell(i).toString();
         console.log(data);
     }
-    
+})
+
+//*fetching multiple data - keeping Column constant
+test.only("fetching multiple data - Column",async ({page}) => {
+    let book=await new excel.Workbook();
+    await book.xlsx.readFile(path.join(__dirname,"../Data Driven Testing/multipledata.xlsx"))
+    let sheet=await book.getWorksheet("Sheet2");
+    for(let i=1; i<=5;i++)
+    {
+        let data=await sheet.getRow(i).getCell(1).toString();
+        console.log(data);
+    }
 })
