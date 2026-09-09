854b637c8ea2c1ca83f637162986e63431ba1e49
soundaryavsb <dsoundarya1998@gmail.com>
2026-09-09 08:19:31 +0530

    POM & DDT | Login | automation Test Login

--- tests/automationTestLogin.spec.js
+++ tests/automationTestLogin.spec.js
@@ -1,6 +1,7 @@
 import {test} from "@playwright/test"
 import testlogin from "../POM/Test_Login.js"
-
+import testloginddt from "../Data Driven Testing/TALogin.json"
+//*login with pom
 test("Login Test automation website",async ({page}) => {
     let loginobj=new testlogin(page);
     await page.goto("https://practicetestautomation.com/practice-test-login/");
@@ -8,6 +9,22 @@ test("Login Test automation website",async ({page}) => {
     await loginobj.usernamef.fill("student");
     // await page.waitForTimeout(1000);
     await loginobj.passwordf.fill("Password123");
+    // await page.waitForTimeout(1000);
+    await loginobj.submit.click();
+    await page.waitForTimeout(3000);
+})
+
+//*login with pom & DDT
+test.only("Login Test automation website with POM and DDT",async ({page}) => {
+    let loginobj=new testlogin(page);
+    let url=testloginddt.url;
+    let usn=testloginddt.username;
+    let pwd=testloginddt.password;
+    await page.goto(url);
+    // await page.waitForTimeout(1000);
+    await loginobj.usernamef.fill(usn);
+    // await page.waitForTimeout(1000);
+    await loginobj.passwordf.fill(pwd);
     // await page.waitForTimeout(1000);
     await loginobj.submit.click();
     await page.waitForTimeout(3000);
